4c065e66a41e8aad51a73789975ae7dfc64fc41b
fuad <fumd03@gmail.com>
2025-01-29 15:09:35 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+let menuIcon = document.querySelector("#menu-icon");
+let navBar = document.querySelector(".navbar");
+let sections = document.querySelectorAll("section");
+let navLink = document.querySelectorAll("header nav a ");
+
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offSet = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+    if (top >= offSet && top < offSet + height) {
+      navLink.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a [href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+};
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navBar.classList.toggle("active");
+};
